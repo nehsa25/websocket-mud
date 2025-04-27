30d11d68afbdaf6cd9b39cfeed2add99d7aab1e6
Jesse Stone <jesse.stone@nehsa.nets>
2025-04-27 04:04:58 -0700

    Fix logging to use default logger and not require passing it around

--- utilities/log_telemetry.py
+++ utilities/log_telemetry.py
@@ -15,13 +15,13 @@ from settings.global_settings import GlobalSettings
 
 
 class LogTelemetryUtility:
-    @staticmethod
-    def get_logger(
-        filename="mud.log",
+    def __init__(
+        self,
+        logger_name="mud",
+        filename="logs/mud.log",
         file_level=GlobalSettings.FILE_LEVEL,
         console_level=GlobalSettings.CONSOLE_LEVEL,
         log_location=GlobalSettings.LOG_LOCATION,
-        tracer=None,
     ):
         """Creates and configures a logger with file and console handlers.
 
@@ -30,60 +30,60 @@ class LogTelemetryUtility:
             file_level (Level): The logging level for the file handler (e.g., Level.DEBUG, Level.INFO).
             console_level (Level): The logging level for the console handler (e.g., Level.DEBUG, Level.INFO).
             log_location (str): The directory where the log file should be created.
-            tracer: The OpenTelemetry tracer object.
-
-        Returns:
-            logging.Logger: The configured logger object.
         """
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.DEBUG)
+        self.logger = logging.getLogger(__name__)  if logger_name == "" else logging.getLogger(logger_name)
+        self.logger.setLevel(logging.DEBUG)
+        self.filename = filename
+        self.file_level = file_level
+        self.console_level = console_level
+        self.log_location = log_location
+        self.tracer = None
 
-        if log_location is None:
-            log_location = os.getcwd()
+        if self.log_location is None:
+            self.log_location = os.getcwd()
 
-        if tracer is None:
-            # Initialize OpenTelemetry
-            resource = Resource.create(
-                {
-                    ResourceAttributes.SERVICE_NAME: "websocket-mud",
-                    ResourceAttributes.SERVICE_VERSION: "0.1.0",
-                }
+        # Initialize OpenTelemetry
+        resource = Resource.create(
+            {
+                ResourceAttributes.SERVICE_NAME: "websocket-mud",
+                ResourceAttributes.SERVICE_VERSION: "0.1.0",
+            }
+        )
+
+        seq_api_key = Secrets.SeqAPIKey
+        headers = {
+            "X-Seq-ApiKey": seq_api_key,
+            "Content-Type": "application/x-protobuf",
+        }
+        try:
+            otlp_exporter = OTLPSpanExporter(
+                endpoint="http://util.nehsa.net:5341/ingest/otlp/v1/traces",
+                headers=headers,
             )
 
-            seq_api_key = Secrets.SeqAPIKey
-            headers = {
-                "X-Seq-ApiKey": seq_api_key,
-                "Content-Type": "application/x-protobuf",
-            }
-            try:
-                otlp_exporter = OTLPSpanExporter(
-                    endpoint="http://util.nehsa.net:5341/ingest/otlp/v1/traces",
-                    headers=headers,
-                )
+        except Exception as e:
+            self.logger.error(f"Error initializing OTLP Exporter: {e}")
+            otlp_exporter = None
 
-            except Exception as e:
-                logger.error(f"Error initializing OTLP Exporter: {e}", logger)
-                otlp_exporter = None
+        if otlp_exporter:
+            tracer_provider = TracerProvider(resource=resource)
 
-            if otlp_exporter:
-                tracer_provider = TracerProvider(resource=resource)
-
-                # Add ConsoleSpanExporter to see spans in the console
-                tracer_provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
-                tracer_provider.add_span_processor(
-                    BatchSpanProcessor(ConsoleSpanExporter())
-                )
-                trace.set_tracer_provider(tracer_provider)
-                tracer = trace.get_tracer(__name__)
+            # Add ConsoleSpanExporter to see spans in the console
+            tracer_provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+            tracer_provider.add_span_processor(
+                BatchSpanProcessor(ConsoleSpanExporter())
+            )
+            trace.set_tracer_provider(tracer_provider)
+            self.tracer = trace.get_tracer(__name__)
 
         # Create file handler
-        log_file_path = os.path.join(log_location, filename)
+        log_file_path = os.path.join(self.log_location, self.filename)
         file_handler = logging.FileHandler(log_file_path)
-        file_handler.setLevel(file_level.value)
+        file_handler.setLevel(self.file_level.value)
 
         # Create console handler
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(console_level.value)
+        console_handler.setLevel(self.console_level.value)
 
         # Create formatter and set it for handlers
         formatter = logging.Formatter(
@@ -93,47 +93,61 @@ class LogTelemetryUtility:
         console_handler.setFormatter(formatter)
 
         # Add handlers to the logger
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
 
-        return logger
-
-    @staticmethod
-    def debug(message, logger):
+    def debug(self, message):
         """Logs a debug message with OpenTelemetry tracing."""
-        LogTelemetryUtility._log(message, logger, logging.DEBUG)
+        self._log(message, logging.DEBUG)
 
-    @staticmethod
-    def info(message, logger):
+    def info(self, message):
         """Logs an info message with OpenTelemetry tracing."""
-        LogTelemetryUtility._log(message, logger, logging.INFO)
+        self._log(message, logging.INFO)
 
-    @staticmethod
-    def warn(message, logger):
+    def warn(self, message):
         """Logs a warning message with OpenTelemetry tracing."""
-        LogTelemetryUtility._log(message, logger, logging.WARNING)
+        self._log(message, logging.WARNING)
 
-    @staticmethod
-    def error(message, logger):
+    def error(self, message):
         """Logs an error message with OpenTelemetry tracing."""
-        LogTelemetryUtility._log(message, logger, logging.ERROR)
+        self._log(message, logging.ERROR)
 
-    @staticmethod
-    def critical(message, logger):
+    def critical(self, message):
         """Logs a critical message with OpenTelemetry tracing."""
-        LogTelemetryUtility._log(message, logger, logging.CRITICAL)
+        self._log(message, logging.CRITICAL)
 
-    @staticmethod
-    def _log(message, logger, level):
+    def _log(self, message, level):
         """Logs a message with OpenTelemetry tracing."""
-        if logger:
-            tracer = trace.get_tracer(__name__)
+        if self.logger:
             current_context = context.get_current()
 
             # Create span
-            with tracer.start_as_current_span(
+            with self.tracer.start_as_current_span(
                 message, context=current_context
             ) as current_span:
                 if current_span is not trace.INVALID_SPAN:
                     current_span.set_attribute("log.message", message)
-                logger.log(level, message)
+                self.logger.log(level, message)
+
+    @staticmethod
+    def get_logger(
+        logger_name="mud",
+        filename="logs/mud.log",
+        file_level=GlobalSettings.FILE_LEVEL,
+        console_level=GlobalSettings.CONSOLE_LEVEL,
+        log_location=GlobalSettings.LOG_LOCATION,
+    ):
+        """Creates and configures a logger with file and console handlers.
+
+        Args:
+            filename (str): The name of the log file.
+            file_level (Level): The logging level for the file handler (e.g., Level.DEBUG, Level.INFO).
+            console_level (Level): The logging level for the console handler (e.g., Level.DEBUG, Level.INFO).
+            log_location (str): The directory where the log file should be created.
+
+        Returns:
+            logging.Logger: The configured logger object.
+        """
+
+        
+        return LogTelemetryUtility(logger_name, filename, file_level, console_level, log_location)
